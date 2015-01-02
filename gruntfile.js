62a41be9fa06255ee63e9955a4d1bbe8e29a7353
Michael Demmer <demmer@jut.io>
2015-01-02 06:59:58 -0800

    update to grunt-dry 0.2.0

--- gruntfile.js
+++ gruntfile.js
@@ -1,7 +1,18 @@
 module.exports = function(grunt) {
     var pkg = grunt.file.readJSON('package.json');
     grunt.initConfig({
-        pkg: pkg
+        gruntDry: {
+            pkg: pkg,
+            deps: {
+                bluebird: {
+                    browserBuild: 'node_modules/bluebird/js/browser/bluebird.js'
+                },
+                chai: {
+                    browserBuild: 'node_modules/chai/chai.js',
+                    testOnly: true
+                }
+            }
+        }
     });
 
     grunt.task.loadNpmTasks('grunt-dry');
